9ab77a60635982b143ae122317b9fee9cf9dfcb5
Ekaterina Feklistova <fkatya501@yandex.ru>
2023-04-24 20:45:51 +0300

    Практическая 6.1

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,67 +1,81 @@
 //функция, которая добвляет класс с ошибкой
-function inputShowErrorClass (input, errorElement) {
-    input.classList.add('popup__input_type_error');
+function inputShowErrorClass ({inputErrorClass}, input, errorElement) {
+    input.classList.add(inputErrorClass);
     errorElement.textContent = input.validationMessage;
     errorElement.classList.add('form__input-error');
 };
 
 //функция, которая убирает класс с ошибкой
-function inputHideErrorClass (input, errorElement) {
-    input.classList.remove('popup__input_type_error');
+function inputHideErrorClass ({inputErrorClass}, input, errorElement) {
+    input.classList.remove(inputErrorClass);
     errorElement.textContent = '';
     errorElement.classList.remove('form__input-error');
 };
 
 //функция, которая проверяет валидность поля
-function checkedValidity (input) {
+function checkedValidity ({ inputErrorClass }, input) {
     const errorElement = document.querySelector(`#error-${input.id}`);
     
     if (! input.validity.valid){
-        inputShowErrorClass(input, errorElement);
+        inputShowErrorClass(inputErrorClass, input, errorElement);
     } else {
-        inputHideErrorClass(input, errorElement);
+        inputHideErrorClass(inputErrorClass, input, errorElement);
     };
 };
 
 //функция для скрывания кнопки
-function disableButton(button){
+function disableButton(inactiveButtonClass, button){
     button.setAttribute('disabled', '');
-    button.classList.add('popup__submit_add');
+    button.classList.add(inactiveButtonClass);
+    
 };
 
 //функция показа кнопки
-function enableButton(button){
+function enableButton(inactiveButtonClass, button){
     button.removeAttribute('disabled');
-    button.classList.remove('popup__submit_add');
+    button.classList.remove(inactiveButtonClass);
 };
 
 //функция проверки показа или скрытия кнопки
-function validityButton(form){
-    const savedButton = document.querySelector('.popup__submit');
+function validityButton({ submitButtonSelector, inactiveButtonClass}, form){
+    const savedButton = document.querySelector(submitButtonSelector);
+    console.log(form);
     if (form.checkValidity()){
-        enableButton(savedButton);
+        enableButton(inactiveButtonClass, savedButton);
     } else {
-        disableButton(savedButton);
+        disableButton(inactiveButtonClass, savedButton);
     };
 };
 
-function enableValidation() {
-    const form = document.querySelector('.popup__form');
-        
-    //отключаем отправку для всех форм
-    form.addEventListener('submit', function(evt){
-        evt.preventDefault();
-    });
-        
-    const inputs = document.querySelectorAll('.popup__input');
-    const inputsArray = Array.from(inputs);
-    inputsArray.forEach(function(input){
-        input.addEventListener('input', () => {
-            checkedValidity(input);
-
-            //кнопка
-            validityButton(form);
+function enableValidation({ formSelector, inputSelector, ...rest}) {
+    const forms = document.querySelectorAll(formSelector);
+    const formsArray = Array.from(forms);
+    formsArray.forEach(function(form){
+        //отключаем отправку для всех форм
+        form.addEventListener('submit', function(evt){
+            evt.preventDefault();
+        });
+        /*validityButton(rest, form);*/
+    
+    
+        const inputs = document.querySelectorAll(inputSelector);
+        console.log(inputs);
+        const inputsArray = Array.from(inputs);
+        inputsArray.forEach(function(input){
+            input.addEventListener('input', () => {
+                checkedValidity(rest, input);
+                //кнопка
+                validityButton(rest, form);
+            });
         });
     });
 };
-enableValidation();
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit',
+    inactiveButtonClass: 'popup__submit_add',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'form__input-error_visible'
+  }); 
